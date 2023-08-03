b2ecda2774d39a195bf537fba4b0eb3336b11826
DESKTOP-UTB7950\USER <songsaemiye@naver.com>
2023-08-03 16:47:50 +0900

    2023-08-03 초기환경세팅

---
+++ src/test/java/com/mupe/MupEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MupEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
