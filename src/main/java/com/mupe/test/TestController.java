2dd1e4636ab4e99b90d9e4ea978d297760671c75
DESKTOP-UTB7950\USER <songsaemiye@naver.com>
2023-08-03 18:41:51 +0900

    초기환경세팅

--- src/main/java/com/mupe/test/TestController.java
+++ src/main/java/com/mupe/test/TestController.java
@@ -1,5 +1,8 @@
 package com.mupe.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,5 +14,20 @@ public class TestController {
 	@RequestMapping("/test1")
 	public String test1() {
 		return "Hello world!";
+	}
+	
+	@RequestMapping("/2")
+	@ResponseBody
+	public Map<String, Object> quiz01_2() {
+		Map<String, Object> scoreMap = new HashMap<>();
+		scoreMap.put("국어", 80);
+		scoreMap.put("수학", 90);
+		scoreMap.put("영어", 85);
+		return scoreMap;
+	}
+	
+	@RequestMapping("/test3")
+	public String test3() {
+		return "test/test1";
 	}
 }
