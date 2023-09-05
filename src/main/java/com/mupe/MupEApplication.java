4b125d806baee12aa476f5fc9c96e34c9503192f
DESKTOP-UTB7950\USER <songsaemiye@naver.com>
2023-09-05 17:11:30 +0900

    war로 말기 위한 준비

--- src/main/java/com/mupe/MupEApplication.java
+++ src/main/java/com/mupe/MupEApplication.java
@@ -2,9 +2,10 @@ package com.mupe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MupEApplication {
+public class MupEApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MupEApplication.class, args);
--- src/main/java/com/mupe/common/FileManagerService.java
+++ src/main/java/com/mupe/common/FileManagerService.java
@@ -17,7 +17,8 @@ public class FileManagerService {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	// 실제 업로드가 된 이미지가 저장될 경로(서버)
-	public static final String FILE_UPLOAD_PATH = "C:\\songsaemi\\mup_e_project\\workspace\\images/";
+	//public static final String FILE_UPLOAD_PATH = "C:\\songsaemi\\mup_e_project\\workspace\\images/";
+	public static final String FILE_UPLOAD_PATH = "/home/ec2-user/images/";
 	
 	// input: MultipartFile(이미지 파일), loginId
 	// output: web image path(String)
